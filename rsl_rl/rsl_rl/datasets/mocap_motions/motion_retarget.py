6f8fae8630c58f83667eb34f49bc8c92cbd9e66b
WoohyunCha <woohyun321@snu.ac.kr>
2024-07-29 18:09:43 +0900

    retargetter updated

--- rsl_rl/rsl_rl/datasets/mocap_motions/motion_retarget.py
+++ rsl_rl/rsl_rl/datasets/mocap_motions/motion_retarget.py
@@ -1,50 +1,129 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 import numpy as np
 
-def parse_mjcf(filepath):
-    tree = ET.parse(filepath)
-    root = tree.getroot()
-    joints = []
-    bodies = {}
+MJCF_file = '/home/cha/isaac_ws/AMP_for_hardware/resources/robots/tocabi/xml/dyros_tocabi.xml'
+
+class MotionRetarget():
+    def __init__(self, source_model_path: str, source_reference: np.ndarray):
+        self.source = source_model_path
+        self.reference = source_reference
+        self.source_bodies, self.source_joints, self.source_edges = self.process_model(self.source)
+        
+        self.print_model(self.source)
+
+    def process_model(self, model_path: str):
+        self.source = model_path
+        tree = etree.parse(self.source)
+        root = tree.getroot()
+        joints = {}
+        bodies = {}
+        edges = {}
 
     # Collect body and joint data
-    for body in root.findall('.//body'):
-        body_name = body.get('name')
-        pos = np.array(body.get('pos', '0 0 0').split(), dtype=float) # default value is 0, 0, 0
-        quat = np.array(body.get('quat', '0 0 0 1').split(), dtype=float)
-        bodies[body_name] = {'pos': pos, 'quat': quat, 'children': []}
+        for body in root.findall('.//body'):
+            body_name = body.get('name')
+            pos = np.array([float(num) for num in body.get('pos', '0 0 0').split()], dtype=float) # default value is 0, 0, 0
+            quat = np.array([float(num) for num in body.get('quat', '1 0 0 0').split()], dtype=float) # default value is 0, 0, 0
+            if body.find('inertial') is not None: # not a dummy
+                inertial = body.find('inertial')
+                mass = np.array([float(num) for num in inertial.get('mass', '0.').split()], dtype=float) # default value is 0, 0, 0
+                com = np.array([float(num) for num in inertial.get('pos', '0 0 0').split()], dtype=float) # default value is 0, 0, 0
+                inertia = np.array([float(num) for num in inertial.get('fullinertia', '0 0 0 0 0 0').split()], dtype=float) # default value is 0, 0, 0
+            else:
+                mass = None
+                com = None
+                inertia = None
 
-        for joint in body.findall('joint'):
-            joint_name = joint.get('name')
-            joint_pos = np.array(joint.get('pos', '0 0 0').split(), dtype=float)
-            joint_axis = np.array(joint.get('axis', '0 0 1').split(), dtype=float)
-            joint_range = np.array(joint.get('range', '-3.14, 3.14').split(), dtype=float)
-            joints.append((body_name, joint_name, joint_pos))
+            body_info = {
+                'position': pos,
+                'quat': quat,
+                'mass': mass,
+                'inertia': inertia,
+                'com': com
+            }
 
-            # Link to parent body
-            if body_name in bodies:
-                bodies[body_name]['children'].append((joint_name, joint_pos))
+            parent = body.getparent()
+            parent_name = parent.get('name')
+            if parent_name is None:
+                parent_name = 'world'
+            
+            if body.find('joint') is not None:
+                for i, joint in enumerate(body.findall('joint')):
+                    assert i == 0, f"Multiple parent link is prohibited, parent link {i} detected."
+                    joint_name = joint.get('name')
+                    joint_pos = np.array([float(num) for num in joint.get('pos', '0 0 0').split()], dtype=float)
+                    joint_axis = np.array([float(num) for num in joint.get('axis', '0 0 1').split()], dtype=float)
+                    joint_type = joint.get('type', 'revolute')
+                    joint_range = np.array([float(num) for num in joint.get('range', '-3.14 3.14').split()], dtype=float)
+                    joint_info = {
+                        'parent' : parent_name,
+                        'child' : body_name,
+                        'position' : joint_pos,
+                        'axis' : joint_axis,
+                        'type' : joint_type,
+                        'range' : joint_range
+                    }
+                    joints[joint_name] = joint_info
+            else: # fixed link
+                joint_name = None             
+            bodies[body_name] = {'parent': parent_name, 'joint': joint_name, 'info' : body_info}
+            if (body_name, parent_name) not in edges:
+                edges[(body_name, parent_name)] = {}
+            if (parent_name, body_name) not in edges:
+                edges[(parent_name, body_name)] = {}
 
-    return bodies, joints
+        # process link lengths
+        for body_name, val in bodies.items():
+            pos = val['info']['position']
+            parent_name = val['parent']
+            edges[(body_name, parent_name)]['length'] = np.linalg.norm(pos, ord=2, axis=-1)
+            edges[(parent_name, body_name)]['length'] = np.linalg.norm(pos, ord=2, axis=-1)
+       
+        
+        return bodies, joints, edges
 
-def compute_kinematics(bodies):
-    # Compute forward kinematics
-    for body_name, body_info in bodies.items():
-        parent_pos = body_info['pos']
-        for child_name, child_pos in body_info['children']:
-            # Child absolute position in base frame coordinates
-            child_absolute_pos = parent_pos + child_pos
-            print(f"{child_name} position relative to base: {child_absolute_pos}")
+    def print_model(self, model_path):
+        bodies, joints, edges = self.process_model(model_path=model_path)
+        print("PRINTING MODEL")
+        print("---------------INFO----------------")
+        for body_name, val in bodies.items():
+            print("Link name : ", body_name)
+            print(val)
+            print("Joint name : ", val['joint'])
+            print(joints[val['joint']]) if val['joint'] is not None else print("None")
+            print("----------------------")
+        print("-------------Link length-----------")
+        for tuple, val in edges.items():
+            print(f"{tuple} : {val}")
 
-            # Link vector calculation (if it has a parent, and is not the root)
-            if 'parent' in body_info:
-                link_vector = child_absolute_pos - bodies[body_info['parent']]['pos']
-                print(f"Link vector from {body_info['parent']} to {child_name}: {link_vector}")
+    def retarget(self, target_model_path: str, target_referece: np.ndarray) -> np.ndarray: # outputs retargetted reference
+        target_bodies, target_joints, target_edges = self.process_model(target_model_path)
+        length_ratios = {}
+        for key, val in target_edges:
+            length_ratios[key] = val/self.source_edges[key]
+        
+        #TODO
+        # 1. FK to get global positions of each joint (we have global pos/ori of base link)
+        # 2. Compute relative position vectors between joints, and scale them by length ratios
+        # 3. Starting from base link, sum up the relative position vectors and transform back to base frame coordinates
+
+
+##########HELPER###########
+def quat_from_angle_axis(angle, axis: np.ndarray):
+    theta = (angle / 2).expand_dims(-1)
+    xyz = normalize(axis) * np.sin(theta)
+    w = np.cos(theta)
+    return quat_unit(np.concatenate((xyz, w), axis=-1))
+
+def normalize(x: np.ndarray, eps: float = 1e-9):
+    return x / np.linalg.norm(x, ord=2, axis=-1, keepdims=True).clip(eps, None)
+
+def quat_unit(a):
+    return normalize(a)
 
 def main():
-    mjcf_file_path = 'path_to_your_mjcf_file.xml'
-    bodies, joints = parse_mjcf(mjcf_file_path)
-    compute_kinematics(bodies)
+    mjcf_file_path = MJCF_file
+    MotionRetarget(mjcf_file_path, None)
 
 if __name__ == '__main__':
     main()
